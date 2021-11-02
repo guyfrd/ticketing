8ec1853b9f66f4960d3f0b1cc30ce15efd3e7e3a
guyfrd <gfried@gsitechnology.com>
2021-11-02 15:31:31 +0200

    current user & signout routes

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/signout', (req, res) => {
-    res.send('Hi')
+router.post('/api/users/signout', (req, res) => {
+    console.log("signout")
+    req.session = null;
+    res.send({});
 });
 
 export { router as signoutRouter } 
